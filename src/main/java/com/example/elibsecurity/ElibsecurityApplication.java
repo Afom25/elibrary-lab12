2d0fcc28732ab1dc9412811e76200e146e894651
HIDRI A <agthidri24@yahoo.com>
2022-10-24 16:32:10 -0500

    this commit will add the first

---
+++ src/main/java/com/example/elibsecurity/ElibsecurityApplication.java
@@ -0,0 +1,13 @@
+package com.example.elibsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElibsecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElibsecurityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/elibsecurity/ElibsecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.elibsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElibsecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
